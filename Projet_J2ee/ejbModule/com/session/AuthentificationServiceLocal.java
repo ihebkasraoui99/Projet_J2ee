9ccfb183f5add29c264394463eb663e78a8357c0
kikou <ihebkesraoui@gmail.com>
2022-12-18 22:24:19 +0100

    iheb kesraoui (a7ad)

--- Projet_J2ee/ejbModule/com/session/AuthentificationServiceLocal.java
+++ Projet_J2ee/ejbModule/com/session/AuthentificationServiceLocal.java
@@ -6,11 +6,16 @@ import com.users.Etudiant;
 import com.users.Utilisateur;
 
 import jakarta.ejb.Local;
+
 @Local
 public interface AuthentificationServiceLocal {
 	public Utilisateur authentifier(String login, String pwd);
+
 	boolean existeEmail(String login);
+
 	void sauvegarderUtilisateur(Utilisateur utilisateur);
+
 	List<Etudiant> listerEtudiants();
-	public Etudiant recherherEtudiant(String nom,String prenom);
+
+	public Etudiant recherherEtudiant(String nom, String prenom);
 }
